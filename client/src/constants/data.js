5cfce9aa4f4720bdf4c74d15c5d1767b64a57b4c
kushal466 <kushalgarttan8585@gmail.com>
2025-02-25 12:10:26 +0530

    Update data.js

--- client/src/constants/data.js
+++ client/src/constants/data.js
@@ -1,5 +1,5 @@
 export const categories = [
-    { id: 1, type: "Music" },
+    {  id: 1, type: "Music" },
     { id: 2, type: "Movies" },
     { id: 3, type: "Sports" },
     { id: 4, type: "Tech" },
