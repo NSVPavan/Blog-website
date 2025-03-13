652c816d678a47834422392c205c38a457305436
kushal466 <kushalgarttan8585@gmail.com>
2025-03-13 16:16:08 +0530

    Update upload.js

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,5 +1,5 @@
 import multer from 'multer';
-import { GridFsStorage  } from 'multer-gridfs-storage';
+import { GridFsStorage } from 'multer-gridfs-storage';
 import dotenv from 'dotenv';
 dotenv.config();
 const username = process.env.DB_USERNAME;
