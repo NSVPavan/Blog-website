2b5edde469130e05736b3c059921f0756f841e16
kushal466 <kushalgarttan8585@gmail.com>
2025-03-17 11:13:28 +0530

    Update route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -4,7 +4,7 @@ import { createPost , updatePost, deletePost, getPost, getAllPosts } from '../co
 import { uploadImage , getImage } from '../controller/image-controller.js';
 import { newComment , getComments, deleteComment } from '../controller/comment-controller.js';
 import { loginUser , singupUser, logoutUser } from '../controller/user-controller.js';
-import { authenticateToken, createNewToken } from '../controller/jwt-controller.js';
+import { authenticateToken , createNewToken } from '../controller/jwt-controller.js';
 
 import upload from '../utils/upload.js';
 
