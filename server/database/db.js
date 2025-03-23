c19fb83da21cda3794db1a70088750fa1beefa8b
kushal466 <kushalgarttan8585@gmail.com>
2025-03-23 15:51:37 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -4,7 +4,7 @@ const Connection = async (username,password) => {
     const URL=`mongodb+srv://${username}:${password}@blog-app.ciyui.mongodb.net/`;
     try {
         await mongoose.connect(URL, { useNewUrlParser: true })
-        console.log(' Database connected successfully');
+        console.log('Database connected successfully');
     } catch (error) {
         console.log('Error while connecting to the database ', error);
     }
